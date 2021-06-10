6bff9448ad58959f5826485004aba439a6f96934
Divyanshi <dwadhwa@worldbank.org>
2021-06-10 16:35:13 -0400

    checking by argument.

--- R/wb_incomeclass_function.R
+++ R/wb_incomeclass_function.R
@@ -19,8 +19,7 @@ wbincomeclass <- function(country = "IND", startyear = 1995, endyear = 2010) {
   last <- max(hist$date)
   all <- unique(hist$wb_code)
 
+  data <- hist[date >= startyear,]
 #  data <- hist[date >= startyear & date <= endyear & wb_code %in% country,]
-
-  data <- hist
 
 }
