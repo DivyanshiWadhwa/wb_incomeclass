6726228926159f019bcddad9a32a664af7b19f38
Divyanshi <dwadhwa@worldbank.org>
2021-06-11 14:37:47 -0400

    trying without DT.

--- R/wb_incomeclass_function.R
+++ R/wb_incomeclass_function.R
@@ -13,13 +13,13 @@ wbincomeclass <- function(country = "IND", startyear = 1995, endyear = 2010) {
 
   hist <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1ose4ar0k3Bfty_fLwYcOfIBKsYv_3GAqiJD41swczOA/edit#gid=744413969")
 
-  hist <- data.table::setDT(hist)
+#  hist <- data.table::setDT(hist)
 
   first <- min(hist$date)
   last <- max(hist$date)
   all <- unique(hist$wb_code)
 
-  data <- hist[date < endyear,]
+  data <- hist[datayear < endyear,]
 #  data <- hist[date >= startyear & date <= endyear & wb_code %in% country,]
 
 }
